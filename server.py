16eae0ed521b6b312b95f4078ae11dd2362965f9
Matthew John <matthew@dockstudios.co.uk>
2023-02-04 10:31:25 +0000

    Switch to use celary scheduler

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 
 import os
+from time import sleep
 
 from celery import Celery
 import redis
@@ -9,8 +10,8 @@ from jmon.runner import Runner
 app = Celery(
     "server",
     #backend=f"{os.environ.get('DB_TYPE')}://{os.environ.get('DB_USERNAME')}:{os.environ.get('DB_PASSWORD')}@172.18.0.2:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}",
-    backend=f"{os.environ.get('BROKER_TYPE')}://{os.environ.get('BROKER_USERNAME')}:{os.environ.get('BROKER_PASSWORD')}@172.18.0.3:{os.environ.get('BROKER_PORT')}/{os.environ.get('BROKER_INSTANCE')}",
-    broker=f"{os.environ.get('BROKER_TYPE')}://{os.environ.get('BROKER_USERNAME')}:{os.environ.get('BROKER_PASSWORD')}@172.18.0.3:{os.environ.get('BROKER_PORT')}/{os.environ.get('BROKER_INSTANCE')}"
+    backend=f"{os.environ.get('BROKER_TYPE')}://{os.environ.get('BROKER_USERNAME')}:{os.environ.get('BROKER_PASSWORD')}@172.19.0.2:{os.environ.get('BROKER_PORT')}/{os.environ.get('BROKER_INSTANCE')}",
+    broker=f"{os.environ.get('BROKER_TYPE')}://{os.environ.get('BROKER_USERNAME')}:{os.environ.get('BROKER_PASSWORD')}@172.19.0.2:{os.environ.get('BROKER_PORT')}/{os.environ.get('BROKER_INSTANCE')}"
 )
 
 
@@ -18,18 +19,15 @@ app = Celery(
 example_config = {'name': 'Terrareg', 'steps': [{'goto': 'https://local-dev.dock.studio'}, {'check': {'repsonse': 200, 'title': 'Home - Terrareg'}}, {'type': {'id': 'navBarSearchInput', 'text': 'aws'}}, {'click': {'id': 'navBarSearchButton'}}, {'check': {'url': 'https://local-dev.dock.studio/modules/search?q=aws'}}, {'click': {'id': 'modulesearch-trusted.mixedsearch-trusted-result.aws.1.0.0', 'find': {'class': 'module-card-title'}}}]}
 
 
-# @app.on_after_configure.connect
-# def schedule_periodic_tasks(sender, **kwargs):
-#     # Checking weather information every 1 minute
-#     sender.add_periodic_task(0.5 * 60, perform_check.s(example_config))
+@app.on_after_configure.connect
+def schedule_periodic_tasks(sender, **kwargs):
+    # Checking weather information every 1 minute
+    sender.add_periodic_task(15.0, perform_check.s(example_config))
 
 
 @app.task()
 def perform_check(config):
     runner = Runner()
     runner.perform_check(config)
-
-if __name__ == "__main__":
-    while True:
-        test = perform_check.apply_async((example_config, ))
-        test.get()
+    # print("PRetending", config)
+    return True
