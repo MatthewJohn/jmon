4fb30a6bd3e3b224fcd51d75970a46a2f322fa12
Matthew John <matthew@dockstudios.co.uk>
2023-02-17 06:38:35 +0000

    Add UI pages to list runs and start run details page
    Issue #22

--- jmon/api/app.py
+++ jmon/api/app.py
@@ -20,3 +20,13 @@ class FlaskApp:
     def serve_index():
         """Return static index page"""
         return send_file(os.path.join('..', 'static', 'index.html'))
+
+    @app.route('/check')
+    def serve_check():
+        """Return check page"""
+        return send_file(os.path.join('..', 'static', 'check.html'))
+
+    @app.route('/run')
+    def serve_run():
+        """Return run page"""
+        return send_file(os.path.join('..', 'static', 'run.html'))
