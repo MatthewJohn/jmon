2c387713ff1bd38816f7e09061ecde29b4467337
Matthew John <matthew@dockstudios.co.uk>
2023-02-27 16:53:28 +0000

    Add example basic page layout
    Issue #25

---
+++ ui/src/Views/PageLayout/PageLayout.tsx
@@ -0,0 +1,118 @@
+
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Badge, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import DomainIcon from '@mui/icons-material/Domain';
+
+class PageLayout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+    };
+  }
+
+  mdTheme = createTheme();
+  toggleDrawer = () => {
+    this.setState((state) => {return {open: !state.open};});
+  };
+
+  render() {
+    return (
+      <ThemeProvider theme={this.mdTheme}>
+        <Box sx={{ display: 'flex' }}>
+
+          <CssBaseline />
+          <AppBar position="absolute">
+            <Toolbar
+              sx={{
+                pr: '24px', // keep right padding when drawer closed
+              }}
+            >
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.toggleDrawer}
+                sx={{
+                  marginRight: '36px',
+                  ...(this.state.open && { display: 'none' }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                JMon
+              </Typography>
+              <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={this.state.open}>
+            <Toolbar
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1],
+              }}
+            >
+              <IconButton>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <DashboardIcon />
+                </ListItemIcon>
+                <ListItemText primary="Checks" />
+              </ListItemButton>
+
+              <ListItemButton>
+                <ListItemIcon>
+                  <DomainIcon />
+                </ListItemIcon>
+                <ListItemText primary="Environments" />
+              </ListItemButton>
+              <Divider sx={{ my: 1 }} />
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+            <Toolbar />
+
+
+          </Box>
+        </Box>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default PageLayout;
