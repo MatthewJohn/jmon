aca3a204d4d0fd07557b5f08fd9bba1bdc129067
Matthew John <matthew@dockstudios.co.uk>
2023-02-28 08:26:28 +0000

    Update server build to build nodeJS app and place into new static 'ui' directory.
    Remove old static app
    
    Issue #25

--- ui/src/client.tsx
+++ ui/src/client.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "/api/v1",
   headers: {
     "Content-type": "application/json"
   }
