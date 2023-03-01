f75e507abcf32242939db6b21c055bb5069d61e2
Matthew John <matthew@dockstudios.co.uk>
2023-03-01 07:05:35 +0000

    Add task to delete old runs from database
    Issue #30

--- jmon/scheduler.py
+++ jmon/scheduler.py
@@ -19,5 +19,11 @@ app.conf.beat_schedule = {
         'task': 'jmon.tasks.clean_orphaned_checks.clean_orphaned_checks',
         'schedule': 43200.0,
         'args': []
+    },
+    # Clean expired results every hour
+    'remove_expired_runs': {
+        'task': 'jmon.tasks.remove_expired_runs.remove_expired_runs',
+        'schedule': 3600.0,
+        'args': []
     }
 }
--- jmon/worker.py
+++ jmon/worker.py
@@ -5,9 +5,11 @@ from jmon.client_type import ClientType
 import jmon.tasks.perform_check
 import jmon.tasks.update_check_schedules
 import jmon.tasks.clean_orphaned_checks
+import jmon.tasks.remove_expired_runs
 
 
 # Register tasks with celery
 app.task(bind=True)(jmon.tasks.perform_check.perform_check)
 app.task(jmon.tasks.update_check_schedules.update_check_schedules)
 app.task(jmon.tasks.clean_orphaned_checks.clean_orphaned_checks)
+app.task(jmon.tasks.remove_expired_runs.remove_expired_runs)
